a7bee6194c7750a47cd4721fa2acbd7798bf364d
satyam-engagemobile <satyam@engagemobile.com>
2013-10-25 10:51:23 +0530

    Find-Me project added

---
+++ Find-Me/Find-Me/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Find-Me
+//
+//  Created by Satyam Raikar on 25/10/13.
+//  Copyright (c) 2013 CCI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Find-Me/Find-Me/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Find-Me
+//
+//  Created by Satyam Raikar on 25/10/13.
+//  Copyright (c) 2013 CCI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
